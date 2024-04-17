cbcd733a7fb56dfea846e480740f1352fe91c153
Yhudz <125532285+m0mYhudz@users.noreply.github.com>
2024-04-17 19:59:06 +0200

    Update env.js

--- js/env.js
+++ js/env.js
@@ -1,4 +1,4 @@
-const contractAddress = ""; // Paste the address of the NFT collection.
+const 0x4539e54a0821ceAEac9181760AD97cD450eb40AB = ""; // Paste the address of the NFT collection.
 // uncomment the chain you are using:
 const chainId = "0x507"; // Moonbase
 // const chainId = "0x504"; // Moonbeam
